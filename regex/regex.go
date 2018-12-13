01630327f094c8a0b206197803c855220f96bfa5
zhanglonghu <zhanglonghu@ename.com>
2018-12-13 23:25:02 +0800

    爬蟲開始

---
+++ regex/regex.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"regexp"
+)
+
+const test  = `my email is zhanglonghu@gmail.com
+	my second email is hello@gmail.com.cn
+	lllll redis@gmail.com
+`
+
+func main(){
+	re:= regexp.MustCompile(`([a-zA-Z0-9]+)@([a-zA-Z0-9]+)(\.[a-zA-Z0-9]+)(a-zA)`)
+	match := re.FindAllStringSubmatch(test, -1)
+	for _, valu := range match{
+		fmt.Println(valu)
+	}
+	fmt.Println(match)
+}
